84803e6aab070ab48972d53af620242c3de13b6a
pocholo818 <89579920+pocholo818@users.noreply.github.com>
2022-12-02 23:32:09 +0800

    delete working

--- bocfp-api/app.js
+++ bocfp-api/app.js
@@ -26,44 +26,55 @@ app.get('/child', (req, res) => {
 // get specific child
 app.get('/child/:id', (req, res) => {
   connection.query(`SELECT * FROM child WHERE id=${req.params.id}`, (err, rows, fields) => {
-      if (err) throw err
-      res.json(rows[0])
-    })
+    if (err) throw err
+    res.json(rows[0])
+  })
 });
 
 // POST
 // add new child
 app.post('/child', (req, res) => {
-    const {fname, lname, bdate, sex, guardian, contact, address} = req.body;
+  const { fname, lname, bdate, sex, guardian, contact, address } = req.body;
 
-    connection.query(`INSERT INTO child (fname, lname, bdate, sex, guardian, contact, address) 
+  connection.query(`INSERT INTO child (fname, lname, bdate, sex, guardian, contact, address) 
         VALUES ('${fname}', '${lname}', '${bdate}', '${sex}', '${guardian}', '${contact}', '${address}')`, (err, rows, fields) => {
-        if (err) throw err
-      })
-    res.send("success")
+    if (err) throw err
+  })
+  res.send("success")
 });
 
 // PUT
 // update child
 app.put('/childUpdate/:id', (req, res) => {
-  const {id, fname, lname, bdate, sex, guardian, contact, address} = req.body;
+  const { id, fname, lname, bdate, sex, guardian, contact, address } = req.body;
 
   connection.query(`UPDATE child SET fname = '${fname}', lname = '${lname}', bdate = '${bdate}', 
       sex ='${sex}', guardian = '${guardian}', contact = '${contact}', address = '${address}'
       WHERE id=${id}`, (err, rows, fields) => {
-      if (err) throw err
-    })
+    if (err) throw err
+  })
+  res.send("success")
+});
+
+// DELETE
+// delete child
+app.delete('/child/:id', (req, res) => {
+  const { id } = req.params;
+
+  connection.query(`DELETE FROM child WHERE id='${id}'`, (err, rows, fields) => {
+    if (err) throw err
+  })
   res.send("success")
 });
 
 const start = async () => {
-    try {
-      app.listen(port, () =>
+  try {
+    app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
-      );
-    } catch (error) {
-      console.log(error);
-    }
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
--- bocfp/src/components/ChildCard.vue
+++ bocfp/src/components/ChildCard.vue
@@ -7,63 +7,109 @@
 
     <ion-card-content>
       <p>ID: {{ data.id }}</p>
-      <p>Sex: {{ data.sex}}</p>
+      <p>Sex: {{ data.sex }}</p>
       <p>Birth Date: {{ data.bdate }}</p>
       <p>Guardian Contact Number: {{ data.contact }}</p>
       <p>Address: {{ data.address }}</p><br>
 
       <div style="text-align:center;">
-        <ion-button color="success" style="width: 32%;" :router-link="'/child_view/' + data.id"><ion-icon  :icon="eyeOutline"></ion-icon></ion-button>
-        <ion-button color="warning" style="width: 32%;" :router-link="'/child_edit/' + data.id"><ion-icon :icon="createOutline"></ion-icon></ion-button>
-        <ion-button color="danger" style="width: 32%;"><ion-icon :icon="trashOutline"></ion-icon></ion-button>
+        <ion-button color="success" style="width: 32%;" :router-link="'/child_view/' + data.id"><ion-icon
+            :icon="eyeOutline"></ion-icon></ion-button>
+        <ion-button color="warning" style="width: 32%;" :router-link="'/child_edit/' + data.id"><ion-icon
+            :icon="createOutline"></ion-icon></ion-button>
+        <ion-button color="danger" style="width: 32%;" @click="child_delete"><ion-icon
+            :icon="trashOutline"></ion-icon></ion-button>
       </div>
     </ion-card-content>
   </ion-card>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import { 
+import { defineComponent } from 'vue';
+import {
+  IonCard,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardHeader,
+  IonCardContent,
+  IonIcon,
+  alertController,
+  toastController
+} from '@ionic/vue';
+// icons
+import {
+  eyeOutline,
+  createOutline,
+  trashOutline,
+  addOutline
+} from 'ionicons/icons';
+
+export default defineComponent({
+  name: 'ChildCard',
+  components: {
     IonCard,
     IonCardTitle,
     IonCardSubtitle,
     IonCardHeader,
     IonCardContent,
     IonIcon
-  } from '@ionic/vue';
-    // icons
-  import { 
-  eyeOutline, 
-  createOutline,
-  trashOutline,
-  addOutline
-  } from 'ionicons/icons';
-
-  export default defineComponent({
-    name: 'ChildCard',
-    components: {
-      IonCard,
-      IonCardTitle,
-      IonCardSubtitle,
-      IonCardHeader,
-      IonCardContent,
-      IonIcon
-    },
-    setup() {
-      return {
-        eyeOutline,
-        createOutline,
-        trashOutline,
-        addOutline
-      }
-    },
-    props: {
-      data: Object
-    },
-    computed: {
-      name() {
-        return this.data?.fname + ' ' + this.data?.lname
-      }
+  },
+  setup() {
+    return {
+      eyeOutline,
+      createOutline,
+      trashOutline,
+      addOutline
     }
-  });
+  },
+  methods: {
+    async child_delete() {
+      const alert = await alertController.create({
+        header: 'Are you sure you want to delete?',
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          },
+          {
+            text: 'DELETE',
+            role: 'confirm',
+            handler: async () => {
+              const toast = await toastController.create({
+                duration: 1500,
+                position: 'top'
+              })
+
+              const childId = this.data?.id
+              
+              fetch('http://localhost:5000/child/' + childId, {
+                method: 'DELETE'
+              })
+                .then((data) => {
+                  toast.message = 'Success!'
+                })
+                .catch((error) => {
+                  toast.message = error
+                });
+
+              await toast.present();
+            },
+          },
+        ],
+      });
+
+      await alert.present();
+
+      const { role } = await alert.onDidDismiss();
+    }
+  },
+  props: {
+    data: Object
+  },
+  computed: {
+    name() {
+      return this.data?.fname + ' ' + this.data?.lname
+    }
+  }
+});
 </script>
